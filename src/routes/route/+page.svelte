c169706b31f7d43846045b937274792217b8ab40
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-05-06 10:56:27 -0600

    first commit

---
+++ src/routes/route/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+    <h1 class="text-bold">Route</h1>
+    <p>Its working</p>
+</div>
+</div>
