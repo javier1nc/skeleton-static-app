0be66dc6aff6a0a4cfdf01d8a705b4dcb1fb0ea1
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-05-23 12:08:43 -0600

    // FEAT: modal works

---
+++ src/lib/utilities/ModalButton/ModalButton.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { Modal, getModalStore } from '@skeletonlabs/skeleton';
+import type { ModalSettings, ModalComponent, ModalStore } from '@skeletonlabs/skeleton';
+
+const modalStore = getModalStore();
+
+
+const modal: ModalSettings = {
+    type: 'alert',
+    // Data
+    title: 'Example Alert',
+    body: 'This is an example modal.',
+    image: 'https://i.imgur.com/WOgTG96.gif',
+};
+
+
+
+function modalHelpTrigger(): void {
+    modalStore.trigger(modal);
+}
+</script>
+
+:Modal:
+
+<button class="btn variant-filled" on:click={modalHelpTrigger}>
+    <span class="indent-1">Clck</span>
+</button>
